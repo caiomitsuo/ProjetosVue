877d47b43a6ac8ab2c196d67579583094c66ed66
caio <caio.lima@edu.unifil.br>
2021-01-25 15:06:20 -0300

    Revert "Revert "Api ja retorna dados para o front filtrados""
    This reverts commit 4f3b2e896576f7590885010af9224715b978f632.

--- agrotech/pages/index.vue
+++ agrotech/pages/index.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <table>
-      <tbody>
-        <tr v-for="dado in dados" :key="dado.id">
-          <td>{{dado.id}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+ <div>
+   <ul>
+     <li v-for="(user,u) in users" :key="u">
+       {{user.culture}}
+     </li>
+   </ul>
+ </div>
 </template>
 
 <script>
@@ -15,19 +13,19 @@ import axios from "axios";
 export default {
   data() {
     return {
-        dados: Array,
+      users: [],
     };
   },
   mounted() {
-    this.getDados();
+    this.getUsers();
   },
   methods: {
-    async getDados() {
-        const response = await axios.get("https://run.mocky.io/v3/2a793688-2254-4663-8d2a-0b1845f16777")
-        .then(response => {
-          this.dados = response.data;
-        })
-        .catch((e) => console.log(e));
+    getUsers() {
+      axios
+        .get("https://run.mocky.io/v3/2a793688-2254-4663-8d2a-0b1845f16777")
+        .then((response) => {
+          this.users = response.data.content;
+        });
     },
   },
 };
