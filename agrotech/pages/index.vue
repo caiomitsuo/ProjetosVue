5d88ff4be4ede207ded0558779ac6340e7817151
caio <caio.lima@edu.unifil.br>
2021-01-25 10:23:06 -0300

    Instalado axios e chamada da api

---
+++ agrotech/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="dado in dados" :key="dado.id">
+          <td>{{dado.id}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+        dados: Array,
+    };
+  },
+  mounted() {
+    this.getDados();
+  },
+  methods: {
+    async getDados() {
+        const response = await axios.get("https://run.mocky.io/v3/2a793688-2254-4663-8d2a-0b1845f16777")
+        .then(response => {
+          this.dados = response.data;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
+</script>
+
+<style></style>
