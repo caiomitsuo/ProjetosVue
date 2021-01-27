27fa60e194b8bc6070da969558ec320aff92be56
caio <caio.lima@edu.unifil.br>
2021-01-27 12:06:12 -0300

    Projeto finalizado

--- agrotech/pages/index.vue
+++ agrotech/pages/index.vue
@@ -1,87 +1,33 @@
 <template>
-  <div class="card">
-    <div class="card-logo">
-      <h1 class="card-logo__title">Agrotech</h1>
-    </div>
-    <div class="card-content">
-      <ul class="card-column">
-        <strong class="card-content__title">Cultura</strong>
-        <li
-          v-for="(user, culture) in users"
-          :key="culture"
-          class="card-content__culture"
-        >
-          <p class="card-content__soja">{{ user.culture }}</p>
-        </li>
-      </ul>
-
-      <ul class="card-column">
-        <strong class="card-content__title">Umidade</strong>
-        <li
-          v-for="(user, impurity) in users"
-          :key="impurity"
-          class="card-content__culture"
-        >
-          <p class="card-content__humidity">{{ user.humidity }}</p>
-        </li>
-      </ul> 
-
-      <ul class="card-column">
-        <strong class="card-content__title">Colheita</strong>
-        <li
-          v-for="(user, cropYear) in users"
-          :key="cropYear"
-          class="card-content__culture"
-        >
-          <p class="card-content__colheita">{{ user.cropYear }}</p>
-        </li>
-      </ul>
-
-      <ul class="card-column">
-        <strong class="card-content__title">Impureza</strong>
-        <li
-          v-for="(user, branch) in users"
-          :key="branch"
-          class="card-content__culture"
-        >
-          <p class="card-content__impurity">{{ user.impurity }}</p>
-        </li>
-      </ul>
-
-      <ul class="card-column">
-        <strong class="card-content__title">Pedido</strong>
-        <li
-          v-for="(user, delivery) in users"
-          :key="delivery"
-          class="card-content__culture"
-        >
-          <p class="card-content__delivery">{{ user.deliveryId }}</p>
-        </li>
-      </ul>
-
-      <ul class="card-column">
-        <strong class="card-content__title">Motorista</strong>
-        <li
-          v-for="(user, branch) in users"
-          :key="branch"
-          class="card-content__culture"
-        >
-          <p class="card-content__driver">{{user.driver}}</p>
-        </li>
-      </ul>
-
-      <ul class="card-column">
-        <strong class="card-content__title">Filial</strong>
-        <li
-          v-for="(user, filial) in users"
-          :key="filial"
-          class="card-content__culture"
-        >
-          <p class="card-content__driver">{{user.branch}}</p>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <table>
+    <tr class="table-title">
+      <th class="table-title">Cultura</th>
+      <th class="table-title">Impureza</th>
+      <th class="table-title">Colheita</th>
+      <th class="table-title">Local</th>
+      <th class="table-title">Pedido</th>
+    </tr>
+    <tr v-for="(user, c) in users" :key="c">
+      <td
+        class="table-content"
+        :class="`table-content--${user.culture.toLowerCase()}`"
+      >
+        {{ user.culture }}
+      </td>
+      <td class="table-content">
+        {{ user.impurity }}
+      </td>
+      <td class="table-content">
+        {{ user.humidity }}
+      </td>
+      <td class="table-content">
+        {{ user.branch }}
+      </td>
+      <td class="table-content">
+        {{ user.deliveryId }}
+      </td>
+    </tr>
+  </table>
 </template>
 <script>
 import axios from "axios";
