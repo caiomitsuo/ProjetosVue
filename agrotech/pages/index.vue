28588654a9267a47c30a1ea83cc1e6914f9c1621
caio <caio.lima@edu.unifil.br>
2021-01-27 10:23:19 -0300

    css finalizado

--- agrotech/pages/index.vue
+++ agrotech/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="card">
+    <div class="card-logo">
+      <h1 class="card-logo__title">Agrotech</h1>
+    </div>
     <div class="card-content">
       <ul class="card-column">
-        <strong class="card-content-title">Cultura</strong>
+        <strong class="card-content__title">Cultura</strong>
         <li
           v-for="(user, culture) in users"
           :key="culture"
@@ -13,57 +16,68 @@
       </ul>
 
       <ul class="card-column">
-        <strong class="card-content-title">Impureza</strong>
+        <strong class="card-content__title">Umidade</strong>
         <li
           v-for="(user, impurity) in users"
           :key="impurity"
           class="card-content__culture"
         >
-          <p class="card-culture__impurity">{{ user.impurity }}</p>
+          <p class="card-content__humidity">{{ user.humidity }}</p>
         </li>
-      </ul>
+      </ul> 
 
       <ul class="card-column">
-        <strong class="card-content-title">Colheita</strong>
+        <strong class="card-content__title">Colheita</strong>
         <li
           v-for="(user, cropYear) in users"
           :key="cropYear"
           class="card-content__culture"
         >
-          <p class="card-culture__colheita">{{ user.cropYear }}</p>
+          <p class="card-content__colheita">{{ user.cropYear }}</p>
         </li>
       </ul>
 
       <ul class="card-column">
-        <strong class="card-content-title">Local</strong>
+        <strong class="card-content__title">Impureza</strong>
         <li
           v-for="(user, branch) in users"
           :key="branch"
           class="card-content__culture"
         >
-          <p class="card-branch__local">{{ user.branch }}</p>
+          <p class="card-content__impurity">{{ user.impurity }}</p>
         </li>
       </ul>
 
       <ul class="card-column">
-        <strong class="card-content-title">Pedido</strong>
+        <strong class="card-content__title">Pedido</strong>
         <li
           v-for="(user, delivery) in users"
           :key="delivery"
           class="card-content__culture"
         >
-          <p class="card-culture__title">{{ user.deliveryId }}</p>
+          <p class="card-content__delivery">{{ user.deliveryId }}</p>
         </li>
       </ul>
 
       <ul class="card-column">
-        <strong class="card-content-title">Status</strong>
+        <strong class="card-content__title">Motorista</strong>
         <li
-          v-for="(user, status) in users"
-          :key="status"
+          v-for="(user, branch) in users"
+          :key="branch"
           class="card-content__culture"
         >
-          <p class="card-content__btn">Entregue</p>
+          <p class="card-content__driver">{{user.driver}}</p>
+        </li>
+      </ul>
+
+      <ul class="card-column">
+        <strong class="card-content__title">Filial</strong>
+        <li
+          v-for="(user, filial) in users"
+          :key="filial"
+          class="card-content__culture"
+        >
+          <p class="card-content__driver">{{user.branch}}</p>
         </li>
       </ul>
     </div>
@@ -83,7 +97,7 @@ export default {
   methods: {
     getUsers() {
       axios
-        .get("https://run.mocky.io/v3/2a793688-2254-4663-8d2a-0b1845f16777")
+        .get("https://run.mocky.io/v3/22a9cdc9-ee02-4a10-b27f-568465c3b85b")
         .then((response) => {
           this.users = response.data.content;
         });
