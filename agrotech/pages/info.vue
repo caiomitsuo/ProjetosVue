0acf9924297026570e0b1a7c3a13766b9ac5c21b
caio <caio.lima@edu.unifil.br>
2021-01-28 14:04:04 -0300

    css finalizado e tratamento de valor feito

--- agrotech/pages/info.vue
+++ agrotech/pages/info.vue
@@ -1,20 +1,28 @@
 <template>
-  <table>
-    <tr>
-      <th>Cultura</th>
-      <th>Ano</th>
-      <th>A fixar</th>
-      <th>Entregue</th>
-      <th>Fixado</th>
-    </tr>
-    <tr v-for="(item, index) in items" :keu="index">
-      <td>{{ item.culture }}</td>
-      <td>{{ item.crop }}</td>
-      <td>{{ item.toFix }}</td>
-      <td>{{ item.delivered }}</td>
-      <td>{{ item.fixed }}</td>
-    </tr>
-  </table>
+  <div class="content">
+    <h1 class="content-title">Informações sobre o Produto</h1>
+    <table>
+      <tr class="header">
+        <th class="header-title">Cultura</th>
+        <th class="header-title">Ano</th>
+        <th class="header-title">A fixar</th>
+        <th class="header-title">Entregue</th>
+        <th class="header-title">Fixado</th>
+      </tr>
+      <tr v-for="(item, index) in items" :key="index">
+        <td
+          class="table-info"
+          :class="`table-info--${item.culture.toLowerCase()}`"
+        >
+          {{ item.culture }}
+        </td>
+        <td class="table-info">{{ item.crop }}</td>
+        <td class="table-info">{{ item.toFix | formatValor}}</td>
+        <td class="table-info">{{ item.delivered | formatValor }}</td>
+        <td class="table-info">{{ item.fixed | formatValor }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -23,6 +31,13 @@ export default {
     return {
       items: [],
     };
+  },
+  filters: {
+    formatValor(item) {
+      return Intl.NumberFormat({ maximumSignificantDigits: 3 }).format(
+        item
+      );
+    },
   },
   mounted() {
     this.getItem();
@@ -38,4 +53,4 @@ export default {
   },
 };
 </script>
-<style></style>
+<style lang="scss" src="../assets/styles/pages/info.scss" scoped></style>
